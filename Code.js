b99a52654b84cec2a6b8cb70d128b20a5889c094
Silver <silver@hopla.online>
2019-05-08 12:20:28 +0800

    webapp-beta

--- Code.js
+++ Code.js
@@ -13,10 +13,6 @@ global.deleteAllTriggers = deleteAllTriggers;
 global.purgeGmail = purgeGmail;
 global.labelPurge = labelPurge;
 
-function test() {
-  Logger.log(1);
-}
-
 function getSettings() {
   Logger.log(userProperties.getProperty("labels_to_purge"));
   Logger.log(userProperties.getProperty("purge_checkFrequency_HOUR"));
@@ -132,12 +128,13 @@ function purgeGmail() {
   var processedLabels = 0;
 
   try {
-    var labels = userProperties.getProperty("labels_to_purge");
-    if (labels) {
-      labels = JSON.parse(labels);
-      for (var key in labels) {
-        Logger.log("Purging [" + key + "]");
-        var deleted = labelPurge(key, labels[key]);
+    var universal_never_purge = _JSONPARSE(userProperties.getProperty("labels_never_purge"));
+    var labels_to_purge = _JSONPARSE(userProperties.getProperty("labels_to_purge"));
+    if (labels_to_purge) {
+      for (var i in labels_to_purge) {
+        var oPurge = labels_to_purge[i];
+        Logger.log("Purging [" + oPurge.purge_label + "]");
+        var deleted = labelPurge(oPurge, universal_never_purge);
         if (deleted) deletedThreads += deleted;
         processedLabels += 1;
       }
@@ -149,7 +146,39 @@ function purgeGmail() {
   }
 }
 
-function labelPurge(label, purgeafter) {
+function test() {
+  var deletedThreads = 0;
+  var processedLabels = 0;
+
+  try {
+    var all_purge = JSON.parse('{"labels_to_purge":[{"purge_label":"HOPLA Emails","older_than":"1","labels_never_purge":["ignore"],"purge_important":true,"purge_starred":false},{"purge_label":"Spam","older_than":"2","labels_never_purge":["ignore"],"purge_important":false,"purge_starred":true}],"labels_never_purge":["Auto Replies"],"purge_checkFrequency_HOUR":5,"status":"disabled"}');
+    var labels_to_purge = all_purge.labels_to_purge;
+    if (labels_to_purge && labels_to_purge.length) {
+      for (var i in labels_to_purge) {
+        var oPurge = labels_to_purge[i];
+        Logger.log("Purging [" + oPurge.purge_label + "]");
+        var deleted = labelPurge(oPurge);
+        if (deleted) deletedThreads += deleted;
+        processedLabels += 1;
+      }
+      // for (var key in labels) {
+      //   Logger.log("Purging [" + key + "]");
+      //   var deleted = labelPurge(key, labels[key]);
+      //   if (deleted) deletedThreads += deleted;
+      //   processedLabels += 1;
+      // }
+
+      return "Processed " + processedLabels + " label(s). Deleted " + deletedThreads + " thread(s).";
+    }
+  } catch (e) {
+    Logger.log(e);
+  }
+}
+
+// function labelPurge(label, purgeafter) {
+function labelPurge(oPurge, aNeverPurge) {
+  var label = oPurge.purge_label; // target label
+  var purgeafter = oPurge.older_than; // days
   if (label === "") {
     return 1;
   }
@@ -161,8 +190,26 @@ function labelPurge(label, purgeafter) {
 
   var search = "label:" + label + " before:" + purge;
 
+  if (oPurge.labels_never_purge && oPurge.labels_never_purge.length) {
+    oPurge.labels_never_purge.forEach(function (value) {
+      search += ' -label:' + value;
+    });
+  }
+
+  if (aNeverPurge && aNeverPurge.length) {
+    aNeverPurge.forEach(function (value) {
+      search += ' -label:' + value;
+    });
+  }
+
+  if (!oPurge.purge_important) search += ' -label:important';
+  if (!oPurge.purge_starred) search += ' -label:starred';
+
+  Logger.log("Your query: " + search);
+  // return;
   // This will create a simple Gmail search
   // query like label:Newsletters before:10/12/2012
+  // eslint-disable-next-line no-unreachable
   var deletedThreads = 0;
   try {
     // We are processing 100 messages in a batch to prevent script errors.
@@ -173,7 +220,7 @@ function labelPurge(label, purgeafter) {
 
     // For large batches, create another time-based trigger that will
     // activate the auto-purge process after 'n' minutes.
-
+    Logger.log("Found: " + threads.length);
     if (threads.length === 100) {
       ScriptApp.newTrigger("purgeGmail")
         .timeBased()
